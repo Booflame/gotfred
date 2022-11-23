332b032140dd50f779c42e7e75be2096a7e64b97
Nichlas Løkken <Loekken.n@gmail.com>
2022-11-23 08:45:24 +0100

    boop

--- src/Components/Burger.js
+++ src/Components/Burger.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 export default function Burger (){
 
-    const svg = 20;
+    const width = 20;
+    const topLine = width/2;
+    const botLine = topLine/2 + topLine;
     const [classToggle, setClassToggle] = useState(false);
 
     function handleClick() {
@@ -11,10 +13,10 @@ export default function Burger (){
 
     return(
         <>
-            <svg className={classToggle ? "newburger active" : "newburger"} onClick={handleClick} viewBox={`0 0 ${svg} ${svg}`} width={svg} height={svg} stroke="black">
-                <path className="line top" d={`M 0 5 h ${svg}`}/>
-                <path className="line mid" d={`M 0 10 h ${svg}`}/>
-                <path className="line bot" d={`M 0 15 h ${svg}`}/>
+            <svg className={classToggle ? "newburger active" : "newburger"} onClick={handleClick} viewBox={`0 0 ${width} ${width}`} width={width} height={width} stroke="black">
+                <path className="line top" d={`M 0 5 h ${topLine}`}/>
+                <path className="line mid" d={`M 0 10 h ${width}`}/>
+                <path className="line bot" d={`M 0 15 h ${botLine}`}/>
             </svg>
         </>
     )
