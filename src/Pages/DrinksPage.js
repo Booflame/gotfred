cfded7a0b38fb485c53d58adcfff5c35f192ae76
Laura Fast <lfast033@gmail.com>
2022-12-17 12:25:56 +0100

    Tilføjet billeder til vendor og drinks page

--- src/Pages/DrinksPage.js
+++ src/Pages/DrinksPage.js
@@ -1,41 +1,49 @@
 import Banner from "../Components/Banner";
 import Priceitem from "../Components/Priceitem";
+import kaffemaskine from "../assets/images/kaffemaskine.jpg";
+import varmdrik from "../assets/images/varmdrik.jpg";
+import kolddrik from "../assets/images/kolddrik.jpg";
 
 export default function DrinksPage() {
 
-    return(
+    return (
         <>
-            <Banner name="Koldt & Varmt" image="banner_kage"/>
+            <Banner name="Koldt & Varmt" image="banner_kage" />
             <div className="wrapper">
                 <section className="section-intro">
                     <div className="title-box">
                         <h2>Varme drikke</h2>
                     </div>
+                    <img src={kaffemaskine} alt="kaffemaskine" />
                     <p>Kaffe og kage går hånd i hånd, og derfor sørger vi for at du får den bedste kaffe som makker til din kage. Vi tilbyder friskbrygget kaffe samt andre varme drikke du kan lune dig på.</p>
                 </section>
                 <section className="price-section">
                     <div className="price-container">
-                        <Priceitem name="Americano" price="38"/>
-                        <Priceitem name="Flat White" price="39"/>
-                        <Priceitem name="Cappuccino" price="42"/>
-                        <Priceitem name="Cafe Latte" price="44"/>
+                        <Priceitem name="Americano" price="38" />
+                        <Priceitem name="Flat White" price="39" />
+                        <Priceitem name="Cappuccino" price="42" />
+                        <Priceitem name="Cafe Latte" price="44" />
                     </div>
+                </section>
+                <img style={{ marginBottom: "20px" }} src={varmdrik} alt="latteogbanoffee" />
+                <section className="price-section">
                     <div className="price-container">
-                        <Priceitem name="Hot Chocolate" price="42"/>
-                        <Priceitem name="Matcha Latte" price="46"/>
-                        <Priceitem name="Chai Latte" price="48"/>
+                        <Priceitem name="Hot Chocolate" price="42" />
+                        <Priceitem name="Matcha Latte" price="46" />
+                        <Priceitem name="Chai Latte" price="48" />
                     </div>
                 </section>
                 <section className="section-intro">
                     <div className="title-box">
                         <h2>Kolde drikke</h2>
                     </div>
+                    <img src={kolddrik} alt="aeblebrus" />
                     <p>Hos Gotfred kan du også slukke tørsten med læskende lemonader og æblebrus fra Bryggeriet Æ. Smag de lækre varianter der går som et smukt kompliment til vores søde og friske kager. </p>
                 </section>
                 <section className="price-section">
                     <div className="price-container">
-                        <Priceitem name="Lemonade" price="30"/>
-                        <Priceitem name="Aeblebrus" price="30"/>
+                        <Priceitem name="Lemonade" price="30" />
+                        <Priceitem name="Aeblebrus" price="30" />
                     </div>
                 </section>
             </div>
--- src/Pages/VendorsPage.js
+++ src/Pages/VendorsPage.js
@@ -1,18 +1,22 @@
 import Banner from "../Components/Banner";
+import moodcoffee from "../assets/images/moodcoffee.jpg";
+import leluneceramics from "../assets/images/leluneceramics.jpg";
+import bryggerietae from "../assets/images/bryggerietae.jpg";
 
 
 export default function VendorsPage() {
 
-    return(
+    return (
         <>
-            <Banner name="Leverandøre" image="banner_kage"/>
+            <Banner name="Leverandører" image="banner_kage" />
             <div className="wrapper">
                 <section className="section-intro">
                     <div className="title-box">
                         <h2>Mood Coffee</h2>
                     </div>
+                    <img src={moodcoffee} alt="kaffeblend" />
                     <p>Ligesom Gotfred, er Mood Coffee Roasters en virksomhed der går op i at være nede på jorden.
-                        De holder deres fokus på det simple og det vigtigste - at lave god kaffe. 
+                        De holder deres fokus på det simple og det vigtigste - at lave god kaffe.
                         Derfor kan du hos Gotfred opleve de forskellige subtile smagsnuancer fra Mood Coffees down-to-earth blend.
                     </p>
                     <p>Du kan lære mere om Mood Coffee Roasters på deres hjemmeside: <a href="https://moodcoffeeroasters.dk/">moodcoffeeroasters.dk/</a></p>
@@ -21,7 +25,8 @@ export default function VendorsPage() {
                     <div className="title-box">
                         <h2>Le Lune Ceramics</h2>
                     </div>
-                    <p>Når du bestiller en kaffe hos Gotfred Aarhus, får du den serveret i de smukkeste krus fra Le Lune Ceramics. 
+                    <img src={leluneceramics} alt="keramikkop" />
+                    <p>Når du bestiller en kaffe hos Gotfred Aarhus, får du den serveret i de smukkeste krus fra Le Lune Ceramics.
                         Den håndlavede keramik lavet af Caroline på eget værksted, er unikke og håndmalede kreationer som kun laves i begrænset opslag.
                     </p>
                     <p>Se og lær mere om Le Lune Ceramics på deres Instagram: <a href="https://www.instagram.com/leluneceramics">leluneceramics instagram</a></p>
@@ -30,6 +35,7 @@ export default function VendorsPage() {
                     <div className="title-box">
                         <h2>Bryggeriet Æ</h2>
                     </div>
+                    <img src={bryggerietae} alt="læskedrikke" />
                     <p>I vores kølemontre finder du friske lemonader og æblebrus fra Bryggeriet Æ.
                         Bryggeriet Æ producerer deres moste fra fynske æbler, hvor det fynske klima og jordbunden gør æbletræer glade. Alt er lavet med ærlighed og kærlighed.
                     </p>
