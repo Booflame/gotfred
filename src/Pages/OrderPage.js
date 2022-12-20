f2af744ec972902965a19658212adf6223edf4b0
Nichlas Løkken <Loekken.n@gmail.com>
2022-12-20 10:47:17 +0100

    order texture

--- src/Pages/OrderPage.js
+++ src/Pages/OrderPage.js
@@ -79,11 +79,11 @@ export default function OrderPage() {
 
     return (
         <>
-            <div className="wrapper">
-                <section className="section-intro">
-                    <div className="title-box">
-                        <h2 className="heading-title">Bestilling</h2>
-                    </div>
+            <section className="section-intro">
+                <div className="title-box">
+                    <h2 className="heading-title">Bestilling</h2>
+                </div>
+                <div className="wrapper">
                     <p>Hos Gotfred Aarhus kan du bestille og afhente vores lækre portionskager samt forudbestille vores Petite Mix.</p>
                     <p>Læs hvordan herunder og bestil dine kager hurtigt og nemt.</p>
                     <div className="grid-d2">
@@ -103,17 +103,23 @@ export default function OrderPage() {
                             </p>
                         </div>
                     </div>
-                </section>
-                <form onSubmit={handleSubmit} className="">
-                    <section className="pick-cake-container">
-                        <h2>1. Vælg dine kager</h2>
-                        <div className="order_texture">
-                            {items.map((item, index) => (
-                                <Orderitem name={item.acf.name} image={item.acf.order_image ? item.acf.order_image : item.acf.image} max={maxAmount} key={index} clickEvent={(e) => handleclick(e)} />
-                            ))}
-                            <Orderitem name="petitemix" image={PetitemixImage} max={5} clickEvent={(e) => handleclick(e)} />
-                        </div>
-                    </section>
+                </div>
+            </section>
+            <form onSubmit={handleSubmit} className="">
+                <div className="order-texture">
+                    <div className="wrapper">
+                        <section className="pick-cake-container">
+                            <h2>1. Vælg dine kager</h2>
+                            <div className="order_texture">
+                                {items.map((item, index) => (
+                                    <Orderitem name={item.acf.name} image={item.acf.order_image ? item.acf.order_image : item.acf.image} max={maxAmount} key={index} clickEvent={(e) => handleclick(e)} />
+                                ))}
+                                <Orderitem name="petitemix" image={PetitemixImage} max={5} clickEvent={(e) => handleclick(e)} />
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                <div className="wrapper">
                     <section>
                         <h2>2. Vælg dato for afhenting</h2>
                         <input onChange={(e) => handleChange(e)} type="date" />
@@ -142,8 +148,8 @@ export default function OrderPage() {
                     <div className="order-button-container">
                         <button className="order-button">Næste</button>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </>
     )
 }
